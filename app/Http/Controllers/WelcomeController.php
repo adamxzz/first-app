d879a560f73df0aa38dfe22cbb811bbba79eed43
adamxzz <adamzaman51@gmail.com>
2022-10-03 15:24:05 +0100

    fixed a bunch of stuff

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class WelcomeController extends Controller
+{
+    public function index() {
+        //fetching data using these methods
+
+        //rawdog SQL Queries 
+        // $users = DB::select('select * from users');
+        // dd($users);
+
+        //Query Builder
+        $users = DB::table('users')->select(['name','email'])->whereNotNull('email')->orderBy('name')->get();
+        dd($users);
+        //Eloquent ORM
+
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,19 @@
-<!DOCTYPE html>
-<html lang="eng">
-    <head>
-        <title>Home</title>
-    </head>
-    <body>
-    <h1>Hello {{ $name }}</h1>
+<x-base>
 
-    </body>
-</html>
+<x-slot name="name">
+     AdamZ
+</x-slot>
+
+<x-slot name="content">
+<h1 class="text-4xl font-bold">Home</h1>
+<p class="mt-4 text-lg"> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+     Quisque venenatis dui ut lorem congue sollicitudin. Aenean vitae sollicitudin lorem, at ullamcorper risus. 
+     Mauris vitae sapien massa. Quisque ullamcorper luctus nulla, id tempus enim lacinia gravida. 
+     Duis bibendum id turpis nec viverra. Fusce at ante condimentum, aliquam erat non, fringilla nunc. 
+     Fusce quam augue, mattis sed feugiat id, vestibulum quis eros. Duis feugiat congue risus, a sodales mauris tempor vel. 
+     Donec aliquet porta dui vel facilisis. Sed in tellus non nibh gravida sagittis. Integer felis magna, ultricies sed quam sit amet, consequat scelerisque libero. 
+     Vestibulum dictum bibendum nunc, eget dictum eros malesuada bibendum. Proin at malesuada mi. 
+     Pellentesque eu eros justo. Aliquam erat volutpat.</p>
+</x-slot>
+
+</x-base>  
