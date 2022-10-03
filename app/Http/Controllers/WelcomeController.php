e34364f58a33903cf938adee5399cc2255052859
adamxzz <adamzaman51@gmail.com>
2022-10-03 16:22:06 +0100

    used eloquent

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -15,9 +16,19 @@ class WelcomeController extends Controller
         // dd($users);
 
         //Query Builder
-        $users = DB::table('users')->select(['name','email'])->whereNotNull('email')->orderBy('name')->get();
-        dd($users);
+        // $users = DB::table('users')->select(['name','email'])->whereNotNull('email')->orderBy('name')->get();
+        // dd($users);
+        
         //Eloquent ORM
+        // $students = Student::select(['name','email'])->whereNotNull('email')->orderBy('name')->get();
+        // dd($students);
+        // foreach($students as $student) {
+        //     echo $student->name. "<br>";
+        // }
 
+        $student = new Student();
+        $student->name = "New";
+        $student->email = "email@email.com";
+        $student->save();
     }
 }
